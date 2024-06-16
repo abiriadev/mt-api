f15e8c9bf8d0c10b321003c866e60611a871fe43
Abiria <abiria.dev@gmail.com>
2024-06-16 16:32:14 +0900

    docs: documentize auth routes

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { OpenAPIHono, createRoute } from '@hono/zod-openapi'
+import { OpenAPIHono } from '@hono/zod-openapi'
 import { signinSchema } from '../models/signin'
 import { signupSchema } from '../models/signup'
 import { authInfoSchema } from '../models/auth-info'
@@ -16,7 +16,17 @@ authRoute.openapi(
 		'/signup',
 		signupSchema,
 		authInfoSchema,
-		sharedOptions,
+		{
+			summary: '회원가입',
+			description: `이메일, 비밀번호로 회원가입. 이메일 중복시 에러`,
+			reqDescription: '회원가입 시 기입하는 정보',
+			resDescription: '발급된 인증 정보',
+			resStatus: 201,
+			errors: {
+				409: '이미 가입된 이메일 사용',
+			},
+			...sharedOptions,
+		},
 	),
 	c => {
 		return c.json({
@@ -31,7 +41,16 @@ authRoute.openapi(
 		'/signin',
 		signinSchema,
 		authInfoSchema,
-		sharedOptions,
+		{
+			summary: '로그인',
+			description: `이메일, 비밀번호로 로그인 시도. 없는 계정이거나 비밀번호가 틀릴 경우 에러`,
+			reqDescription: '로그인 정보',
+			resDescription: '발급된 인증 정보',
+			errors: {
+				401: '등록되지 않은 이메일 또는 틀린 비밀번호 사용',
+			},
+			...sharedOptions,
+		},
 	),
 	c => {
 		return c.json({
