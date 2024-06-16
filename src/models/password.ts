42bafcf46b4224abc37a5c011099061d65d5eb16
Abiria <abiria.dev@gmail.com>
2024-06-16 19:51:21 +0900

    docs: add more example

--- src/models/password.ts
+++ src/models/password.ts
@@ -2,11 +2,16 @@ import { z } from 'zod'
 
 export const passwordSchema = z.string().openapi({
 	description: '비밀번호. 참고: 현재 어떠한 조건도 없음.',
+	example: 'q1w2e3r4',
 })
 
 export const changePasswordSchema = z
 	.object({
-		old: passwordSchema,
-		new: passwordSchema,
+		old: passwordSchema.openapi({
+			description: '이전 비밀번호',
+		}),
+		new: passwordSchema.openapi({
+			description: '새 비밀번호',
+		}),
 	})
 	.openapi('ChangePassword')
