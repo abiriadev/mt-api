fa2b53adbac03a84aa8a6d4eaaf33a5edc41b27a
Abiria <abiria.dev@gmail.com>
2024-06-16 20:18:52 +0900

    feat: setup oas

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,29 @@ const hono = new OpenAPIHono()
 
 hono.doc('/openapi', {
 	openapi: '3.0.1',
-	info: { title: 'Mitigation KR', version: '1.0.0' },
+	info: {
+		title: 'Mitigation KR',
+		version: '0.1.0',
+		description: 'Mitigation KR 백엔드 API',
+		contact: {
+			name: '이승하',
+			url: 'mitigation.kr',
+			email: 'abuse@hanarin.uk',
+		},
+		license: {
+			name: 'MIT',
+		},
+	},
+	servers: [
+		{
+			url: '35.72.107.236',
+			description: 'Hanarin 온프레미스 서버',
+		},
+	],
+	externalDocs: {
+		description: 'Notion 위키',
+		url: 'https://www.notion.so/MITI-Wiki-32b5df14cf214a06bdcd695059734f3c#bc9f3e8270aa43609c8a674e490c28f1',
+	},
 })
 hono.route('/', openapi)
 
