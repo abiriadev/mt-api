57256f122111b7d9cfe93ed85729e31e69bbad66
Abiria <abiria.dev@gmail.com>
2024-06-17 07:02:56 +0900

    fix: syntax error

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -18,14 +18,14 @@ hono.openapi(
 		signupSchema,
 		authInfoSchema,
 		{
+			...sharedOptions,
 			summary: '회원가입',
 			description: `이메일, 비밀번호로 회원가입. 이메일 중복시 에러`,
 			reqDescription: '회원가입 시 기입하는 정보',
 			resDescription: '발급된 인증 정보',
-			resStatus: 201,
+			errors: {
 				409: '이미 가입된 이메일 사용',
 			},
-			...sharedOptions,
 		},
 	),
 	c => {
@@ -42,6 +42,7 @@ hono.openapi(
 		signinSchema,
 		authInfoSchema,
 		{
+			...sharedOptions,
 			summary: '로그인',
 			description: `이메일, 비밀번호로 로그인 시도. 없는 계정이거나 비밀번호가 틀릴 경우 에러`,
 			reqDescription: '로그인 정보',
@@ -49,7 +50,6 @@ hono.openapi(
 			errors: {
 				401: '등록되지 않은 이메일 또는 틀린 비밀번호 사용',
 			},
-			...sharedOptions,
 		},
 	),
 	c => {
