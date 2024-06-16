cbafce550d5aeb61448cf1ea6a19a315538977ac
Abiria <abiria.dev@gmail.com>
2024-06-16 19:07:57 +0900

    docs: document token

--- src/models/auth-info.ts
+++ src/models/auth-info.ts
@@ -2,6 +2,10 @@ import { z } from 'zod'
 
 export const authInfoSchema = z
 	.object({
-		token: z.string(),
+		token: z.string().openapi({
+			description: 'JWT 토큰',
+			example:
+				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+		}),
 	})
 	.openapi('AuthInfo')
