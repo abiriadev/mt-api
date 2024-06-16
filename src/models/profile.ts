7911ef5306719a8b0285f7654ee3658dd5194c18
Abiria <abiria.dev@gmail.com>
2024-06-16 19:49:55 +0900

    docs: add more example

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,13 +1,24 @@
 import { z } from 'zod'
+import { emailSchema } from './email'
 
 export const profileSchema = z
 	.object({
-		name: z.string(),
-		tel: z.string(),
-		email: z.string().email(),
+		name: z.string().openapi({
+			description: '사용자 실명',
+			example: '박헌의',
+		}),
+		tel: z.string().openapi({
+			description: '사용자 전화번호',
+			example: '010-1234-5678',
+		}),
+		email: emailSchema,
 	})
 	.openapi('Profile')
 
 export const updateProfileSchema = profileSchema
+	.omit({
+		name: true,
+		tel: true,
+	})
 	.partial()
 	.openapi('UpdateProfile')
