edefec062ee16d545465713abe92568e1ffd99d3
Abiria <abiria.dev@gmail.com>
2024-06-16 20:05:10 +0900

    docs: add more example

--- src/models/tunnel.ts
+++ src/models/tunnel.ts
@@ -7,6 +7,7 @@ export const tunnelSchema = indexableSchema
 	.extend({
 		name: z.string().openapi({
 			description: '터널 이름',
+			example: '터널 1',
 		}),
 		ip: ipSchema,
 		serverIp: ipSchema,
@@ -27,13 +28,42 @@ export const updateTunnelSchema = createTunnelSchema
 export const tunnelDetailSchema = tunnelSchema
 	.extend({
 		statistics: z.object({
-			bandwidth: z.number().array(),
-			inLast: z.number(),
-			in95: z.number(),
-			inTraffic: z.number(),
-			outLast: z.number(),
-			out95: z.number(),
-			outTraffic: z.number(),
+			bandwidth: z
+				.number()
+				.array()
+				.openapi({
+					description: '트래픽 통계',
+					example: [
+						-2.1213203435596415,
+						-6.928203230275507,
+						-2.1213203435596415,
+						-6.928203230275507,
+						-2.1213203435596415,
+						-6.928203230275507,
+						-2.1213203435596415,
+						-6.928203230275507,
+						-2.1213203435596415,
+						-6.928203230275507,
+					],
+				}),
+			inLast: z.number().openapi({
+				example: 97.58,
+			}),
+			in95: z.number().openapi({
+				example: 97.58,
+			}),
+			inTraffic: z.number().openapi({
+				example: 97.58,
+			}),
+			outLast: z.number().openapi({
+				example: 102.45,
+			}),
+			out95: z.number().openapi({
+				example: 102.45,
+			}),
+			outTraffic: z.number().openapi({
+				example: 102.45,
+			}),
 		}),
 	})
 	.openapi('TunnelDetail')
