bc287958a9377fa152e3dd117cdd7bd36ac59e9a
Abiria <abiria.dev@gmail.com>
2024-06-16 18:01:42 +0900

    docs: add description

--- src/models/signup.ts
+++ src/models/signup.ts
@@ -1,6 +1,10 @@
 import { z } from 'zod'
 
 export const signupSchema = z.object({
-	email: z.string().email(),
-	password: z.string(),
+	email: z.string().email().openapi({
+		description: '이메일 주소',
+	}),
+	password: z.string().openapi({
+		description: '비밀번호',
+	}),
 })
