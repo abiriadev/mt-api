db7ffe97a4726c428574ede95ac68092dc5517f1
Abiria <abiria.dev@gmail.com>
2024-06-16 22:53:08 +0900

    refactor: strict password condition

--- src/models/password.ts
+++ src/models/password.ts
@@ -1,9 +1,14 @@
 import { z } from 'zod'
 
-export const passwordSchema = z.string().openapi({
-	description: '비밀번호. 참고: 현재 어떠한 조건도 없음.',
-	example: 'q1w2e3r4',
-})
+export const passwordSchema = z
+	.string()
+	.min(5)
+	.regex(/[@$!%*#?&]/)
+	.openapi({
+		description:
+			'비밀번호. 참고: 현재 어떠한 조건도 없음.',
+		example: 'q1w2e3r4',
+	})
 
 export const changePasswordSchema = z
 	.object({
