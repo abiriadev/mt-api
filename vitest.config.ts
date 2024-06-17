9b7582a9038d2762694a18b01ac0cf5b5622b99c
Abiria <abiria.dev@gmail.com>
2024-06-17 14:22:07 +0900

    build: reuse tsconfig's path alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+	plugins: [tsconfigPaths()],
 	test: {
 		include: ['./src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
 		setupFiles: './src/vitest.setup.ts',
