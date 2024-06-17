94f28e8d7e38aa0eae42c1c020df660b3df4ad0a
Abiria <abiria.dev@gmail.com>
2024-06-17 13:20:18 +0900

    feat: define package service

---
+++ src/services/package.ts
@@ -0,0 +1,10 @@
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+
+const { name, version } = require('../../package.json') as {
+	name: string
+	version: string
+}
+
+export { name, version }
