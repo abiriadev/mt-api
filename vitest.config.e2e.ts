214a2bd6b1a737a7a973b499eb6dc5ea9c620b6b
Abiria <abiria.dev@gmail.com>
2024-06-17 16:21:45 +0900

    test: setup e2e testing

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	test: {
+		include: [
+			'./tests/**/*.{test,spec}.?(c|m)[jt]s?(x)',
+		],
+	},
+})
