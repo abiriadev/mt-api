67d9682b8c610c6eabe9c8444e098f837a523024
Abiria <abiria.dev@gmail.com>
2024-06-17 10:05:03 +0900

    feat: setup basic tslog service

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,9 +1,9 @@
 import { createMiddleware } from 'hono/factory'
-import { logger } from 'hono/logger'
-
-const honoLogger = logger()
+import { logger } from '@/services/logger'
 
 const loggerMiddleware = createMiddleware((c, next) => {
-	return honoLogger(c, next)
+	logger.info(`[${c.req.method}] ${c.req.path}`)
+
+	return next()
 })
 export { loggerMiddleware as logger }
