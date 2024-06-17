19eed7e32f8017440bed553b28ec02d245550b93
Abiria <abiria.dev@gmail.com>
2024-06-17 11:18:40 +0900

    feat: define config model

---
+++ src/models/config.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod'
+
+const simplePortSchema = z
+	.number()
+	.int()
+	.nonnegative()
+	.lt(1 << 16)
+
+const networkConfigSchema = z.object({
+	host: z.string().default('0.0.0.0'),
+	port: simplePortSchema.default(2727),
+})
+
+const authCredentialsConfigSchema = z.object({
+	jwtSecret: z.string().min(4),
+})
+
+const dbCredentialsConfigSchema = z.object({
+	dbHost: z.string().default('db'),
+	dbPort: simplePortSchema.default(5432),
+	dbUser: z.string().default('postgres'),
+	dbPassword: z.string(),
+	dbDatabase: z.string().default('miti'),
+})
+
+const logConfigSchema = z.object({
+	level: z
+		.enum(['debug', 'info', 'warn', 'error'])
+		.default('info'),
+})
+
+export const configSchema = z
+	.object({})
+	.merge(networkConfigSchema)
+	.merge(authCredentialsConfigSchema)
+	.merge(dbCredentialsConfigSchema)
+	.merge(logConfigSchema)
