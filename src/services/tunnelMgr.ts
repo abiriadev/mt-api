a5c22241b1979cb474a197558f9b2e96a2553de8
Abiria <abiria.dev@gmail.com>
2024-06-18 17:17:05 +0900

    feat: setup error log for tunnels and vnstat

--- src/services/tunnelMgr.ts
+++ src/services/tunnelMgr.ts
@@ -1,7 +1,12 @@
 import shellExecModule from 'shell-exec'
+import { logger } from '@/services/logger.js'
 
 const shellExec = (shellExecModule as any)
 	.default as typeof shellExecModule
+
+const tunnelMgrLogger = logger.getSubLogger({
+	name: 'tunnel',
+})
 
 export interface TunnelUpInfo {
 	interfaceId: string
@@ -14,10 +19,25 @@ export const tunnelUp = async ({
 	serverIp,
 	clientIp,
 }: TunnelUpInfo) => {
-	await shellExec(
+	const tunnelRes = await shellExec(
 		`ip tunnel add ${interfaceId} mode gre local ${serverIp} remote ${clientIp} ttl 255`,
 	)
-	await shellExec(`vnstat --add -i ${interfaceId}`)
+
+	if (tunnelRes.code !== 0) {
+		// TODO: more detailed error message?
+		tunnelMgrLogger.error(tunnelRes)
+
+		// skip vnstat setting when failed
+		return
+	}
+
+	const vnstatRes = await shellExec(
+		`vnstat --add -i ${interfaceId}`,
+	)
+
+	if (vnstatRes.code !== 0) {
+		tunnelMgrLogger.error(vnstatRes)
+	}
 }
 
 export const tunnelDown = async (interfaceId: string) => {
