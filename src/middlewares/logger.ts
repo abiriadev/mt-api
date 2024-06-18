cb9c27d9122d83d2be835336732096e9b1860be7
Abiria <abiria.dev@gmail.com>
2024-06-18 16:44:08 +0900

    feat: show detailed req log

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,9 +1,44 @@
 import { createMiddleware } from 'hono/factory'
 import { logger } from '@/services/logger.js'
 
-const loggerMiddleware = createMiddleware((c, next) => {
-	logger.info(`[${c.req.method}] ${c.req.path}`)
+const loggerMiddleware = createMiddleware(
+	async (c, next) => {
+		let body = null
+		const clonedReq = c.req.raw.clone()
 
-	return next()
-})
+		try {
+			const rawText = await clonedReq.text()
+
+			try {
+				body = JSON.parse(rawText)
+			} catch {
+				body = rawText
+			}
+		} catch {
+			body = '[failed to parse body]'
+		}
+
+		logger.info({
+			method: c.req.method,
+			path: c.req.path,
+			query: c.req.query(),
+			header: c.req.header(),
+			url: c.req.url,
+			body,
+			routes: c.req.matchedRoutes.map(
+				({ path, method, handler }) => ({
+					path,
+					method,
+					handler:
+						handler.name ||
+						(handler.length < 2
+							? '[handler]'
+							: '[middleware]'),
+				}),
+			),
+		})
+
+		await next()
+	},
+)
 export { loggerMiddleware as logger }
